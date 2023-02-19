0d1403bd82d43ae6afbfc985cc66d7cb82869076
km185273 <km185273@ncr.com>
2023-02-19 14:18:23 -0500

    created branch for stashed code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rust Linked List
 
 Gonna try to learn rust by building a linked list. Lets see how it goes...
+
+This branch has a number guessing game. Couldn't figure out Linked list yet, so decided to do this to learn a bit more.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number!");
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    println!("Secret: {secret_num}");
+    let mut guess: String = String::from("0");//String::new();
+    //&mut guess = "0";
+
+    let mut guesses: i32 = 0;
+
+    while guess.trim().parse::<i32>().expect("please give me correct string number!") != secret_num  {
+
+        if !(guesses > 0) {
+            println!("Please input a guess between 1 - 100.");
+        }
+        else if guess.trim().parse::<i32>().expect("please give me correct string number!") > secret_num {
+            println!("Wrong! Try Lower...");
+        }
+        else if guess.trim().parse::<i32>().expect("please give me correct string number!") < secret_num {
+            println!("Wrong! Try Higher...");
+        }
+        guess.clear();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+        guess = String::from(guess.trim());
+
+        guesses += 1;
+    }
+
+    println!("You guessed the correct number, {guess}, in {guesses} trys.")
+
 }
